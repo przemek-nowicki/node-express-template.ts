36a11315be4275c061e179ae2433a3bb23421b25
przemek-nowicki <nowicki.przemek@gmail.com>
2022-05-06 12:56:44 +0200

    move logger log level setup to utils

--- src/core/utils/logger.ts
+++ src/core/utils/logger.ts
@@ -1,4 +1,5 @@
 import winston from 'winston';
+import RnR from 'runtime-node-refresh';
 
 import config from '@config/config';
 
@@ -33,6 +34,41 @@ const logger: winston.Logger = winston.createLogger({
       stderrLevels: ['error'],
     }),
   ],
+});
+
+const avbLogLevels = [
+  'error',
+  'warn',
+  'info',
+  'http',
+  'verbose',
+  'debug',
+  'silly',
+];
+
+let currentLogLevel: number = logger.levels[logger.level];
+
+logger.info(`Logger level is set to ${logger.level}`);
+
+// Refresh log level on runtime
+RnR(() => {
+  if (currentLogLevel < avbLogLevels.length - 1) {
+    currentLogLevel += 1;
+  } else {
+    currentLogLevel = 0;
+  }
+  const found = Object.entries(logger.levels).find(
+    ([, value]) => value === currentLogLevel,
+  );
+  if (found && found.length) {
+    // eslint-disable-next-line
+    console.log(
+      `${new Date().toISOString()} system: Switch logger level from ${
+        logger.level
+      } to ${found[0]}`,
+    );
+    logger.level = found[`${0}`];
+  }
 });
 
 export default logger;
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,4 @@
 import { Server } from 'http';
-import RnR from 'runtime-node-refresh';
-
 import app from '@app';
 import config from '@config/config';
 import logger from '@core/utils/logger';
@@ -8,37 +6,9 @@ import errorHandler from 'core/utils/errorHandler';
 
 const { port } = config;
 
-const setLoggerLevel = (): void => {
-  const avbLogLevels = [
-    'error',
-    'warn',
-    'info',
-    'http',
-    'verbose',
-    'debug',
-    'silly',
-  ];
-
-  let currentLogLevel: number = logger.levels[logger.level];
-
-  logger.info(`Logger level is set to ${logger.level}`);
-
-  // Refresh log level
-  RnR(() => {
-    const found = Object.entries(logger.levels).find(
-      ([, value]) => value === currentLogLevel,
-    );
-    if (found.length) {
-      logger.info(`Switch logger level from ${logger.level} to ${found[0]}`);
-      logger.level = found[`${0}`];
-    }
-    if (currentLogLevel < avbLogLevels.length - 1) {
-      currentLogLevel += 1;
-    } else {
-      currentLogLevel = 0;
-    }
-  });
-};
+const server: Server = app.listen(port, (): void => {
+  logger.info(`Aapplication listens on PORT: ${port}`);
+});
 
 const exitHandler = (): void => {
   if (app) {
@@ -57,11 +27,6 @@ const unexpectedErrorHandler = (error: Error): void => {
     exitHandler();
   }
 };
-
-const server: Server = app.listen(port, (): void => {
-  logger.info(`Aapplication listens on PORT: ${port}`);
-  setLoggerLevel();
-});
 
 process.on('uncaughtException', unexpectedErrorHandler);
 process.on('unhandledRejection', (reason: Error) => {
